0d40b28b124c40c9ec613bbe3aaebb64f15f6489
lysenko <100563799+vombat999@users.noreply.github.com>
2024-02-20 00:36:46 +0600

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,80 @@
+// class  {
+//     public static void main(String args[]){
+        
+//     }
+// }
+
+
+// class Light {
+//     public static void main(String args[]){
+//         int lightspeed;
+//         long days;
+//         long seconds;
+//         long distance;
+//         lightspeed=186000;
+//         days=1000;
+//         seconds=days*24*60*60;
+//         distance= lightspeed * seconds;
+//         System.out.print("за " +days);
+//         System.out.print(" дней свет пройдет около ");
+//         System.out.println(distance+ " миль.");
+
+    
+//     }
+// }
+
+
+// class Area {
+//     public static void main(String args[]){
+//         double pi, r ,a;
+//         r = 10.8;
+//         pi = 3.1416;
+//         a = pi *r *r;
+//         System.out.println("Площадь равна = " + a);
+//     }
+// }
+
+
+// class  CharDemo{
+//     public static void main(String args[]){
+//         char ch1, ch2;
+//         ch1 = 88;
+//         ch2 = 'Y';
+//         System.out.print("ch1 и ch2: ");
+//         System.out.println(ch1 + " " + ch2);
+//     }
+// }
+
+// class CharDemo2 {
+//     public static void main(String args[]){
+//         char ch1;
+//         ch1= 'X';
+//         System.out.println("Ch1 содержит "+ch1);
+//         ch1++;
+//         System.out.println("Ch1 теперь содержит "+ch1);
+        
+//     }
+// }
+
+// class  BoolTest{
+//     public static void main(String args[]){
+//         boolean b;
+//         b = false;
+//         System.out.println("b равно = "+b);
+//         b = true;
+//         System.out.println("b равно = "+b);
+//         if(b) System.out.println("Этот код выполняется");
+//         b = false;
+//         if(b) System.out.println("Этот код не выполняется");
+
+//         System.out.println("10>9 равно " + (10>9));
+
+//     }
+// }
+
+
+class  {
+    public static void main(String args[]){
+        
+    }
+}
